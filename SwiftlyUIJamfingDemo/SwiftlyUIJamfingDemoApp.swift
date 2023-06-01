25bceb0d899592abc3b854f234d7c0f7eda5441a
ethamoos <amosdeane@gmail.com>
2023-06-01 09:11:56 +0100

    Initial Commit

---
+++ SwiftlyUIJamfingDemo/SwiftlyUIJamfingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyUIJamfingDemoApp.swift
+//  SwiftlyUIJamfingDemo
+//
+//  Created by Amos Deane on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlyUIJamfingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
